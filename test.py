00b724d8b9ea3154075477bfb4e2482eea84c2cc
zhguo9 <zhguo9@gmail.com>
2023-12-06 09:22:47 +0800

    上一次提交的结果：在330条上训练，113个epoch后，达到90%准确率。本次把数据集分为259条训练集，72条测试集，进行测试

--- test.py
+++ test.py
@@ -1,9 +1,5 @@
-import re
-x = "CGCGATGATCACCAC57DS"
-print(x[-6:])
-match = re.search(r"\d+",x)
-if match:
-    position = int(match.group(0))
-else:
-    position = 1
-print(position)
+import torch
+
+print(torch.__version__)
+print(torch.cuda.is_available())
+print(torch.version.cuda)
